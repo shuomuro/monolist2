4a5b3986a565e2337ae9c1fd08904922729ede8f
shuomuro <ohmuro.s@gmail.com>
2016-05-18 07:31:54 +0000

    Lesson10 challenge2 before merge Fork

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,8 +11,7 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
-
+      items = RakutenWebService::Ichiba::Item.search(itemCode: params[:item_code])
       item                  = items.first
       @item.title           = item['itemName']
       @item.small_image     = item['smallImageUrls'].first['imageUrl']
@@ -25,8 +24,12 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
+    if params[:type] == "Have"
+      current_user.have(@item)
+    else
+      current_user.want(@item)
+    end
     
-
   end
 
   def destroy
@@ -35,6 +38,11 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
+    if params[:type] == "Have"
+      current_user.unhave(@item)
+    else
+      current_user.unwant(@item)
+    end
 
   end
 end
